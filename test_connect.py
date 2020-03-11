4238b57cad9bfe11d64058a199a553b6a0df85fb
Bradford N. Roarr <bradford_roarr@brown.edu>
2020-03-11 10:32:39 -0400

    Fix test assertions

--- test_connect.py
+++ test_connect.py
@@ -6,7 +6,18 @@ PASSWORD = os.environ['OMERO_PASSWORD']
 HOSTNAME = os.environ.get('hostname', 'pricaimcit.services.brown.edu')
 
 def test_secure_connect():
-    connection_test(USERNAME, PASSWORD, host=HOSTNAME, port=4064, secure=True)
+    '''Connect on secure port should work'''
+    assert connection_test(USERNAME, PASSWORD, host=HOSTNAME, port=4064, secure=True)
+
+def test_insecure_on_secure_port():
+    '''Insecure connection on a secure port should not work'''
+    assert not connection_test(USERNAME, PASSWORD, host=HOSTNAME, port=4064, secure=False)
 
 def test_insecure_connect():
-    connection_test(USERNAME, PASSWORD, host=HOSTNAME, port=4063, secure=False)
+    '''Insecure connections should not work'''
+    assert not connection_test(USERNAME, PASSWORD, host=HOSTNAME, port=4063, secure=False)
+
+def test_secure_on_insecure_port():
+    '''Secure connections on an insecure port should not work'''
+    assert not connection_test(USERNAME, PASSWORD, host=HOSTNAME, port=4063, secure=True)
+
